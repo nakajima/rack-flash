44b5c402fd89fe322d0febd78d652e4b715d1ff2
Chris Causer <c.causer1@physics.ox.ac.uk>
2012-02-14 16:41:42 +0000

    Actually remove the dodgy stuff

--- lib/rack/flash.rb
+++ lib/rack/flash.rb
@@ -1,31 +1,3 @@
-#module Rack
-#  class Builder
-#    attr :ins
-#    def use(middleware, *args, &block)
-#      middleware.instance_variable_set "@rack_builder", self
-#      def middleware.rack_builder
-#        @rack_builder
-#      end
-#      @ins << lambda { |app|
-#        middleware.new(app, *args, &block)
-#      }
-#    end
-#
-#    def run(app)
-#      klass = app.class
-#      klass.instance_variable_set "@rack_builder", self
-#      def klass.rack_builder
-#        @rack_builder
-#      end
-#      @ins << app #lambda { |nothing| app }
-#    end
-#
-#    def leaf_app
-#      ins.last
-#    end
-#  end
-#end
-
 module Rack
   class Flash
     # Raised when the session passed to FlashHash initialize is nil. This
