43d462ea1109eb0111fac4dc132a25194616d3e9
Chris Causer <c.causer1@physics.ox.ac.uk>
2012-02-14 16:30:55 +0000

    Remove builder stuff that seems useless

--- lib/rack/flash.rb
+++ lib/rack/flash.rb
@@ -1,30 +1,30 @@
-module Rack
-  class Builder
-    attr :ins
-    def use(middleware, *args, &block)
-      middleware.instance_variable_set "@rack_builder", self
-      def middleware.rack_builder
-        @rack_builder
-      end
-      @ins << lambda { |app|
-        middleware.new(app, *args, &block)
-      }
-    end
-
-    def run(app)
-      klass = app.class
-      klass.instance_variable_set "@rack_builder", self
-      def klass.rack_builder
-        @rack_builder
-      end
-      @ins << app #lambda { |nothing| app }
-    end
-
-    def leaf_app
-      ins.last
-    end
-  end
-end
+#module Rack
+#  class Builder
+#    attr :ins
+#    def use(middleware, *args, &block)
+#      middleware.instance_variable_set "@rack_builder", self
+#      def middleware.rack_builder
+#        @rack_builder
+#      end
+#      @ins << lambda { |app|
+#        middleware.new(app, *args, &block)
+#      }
+#    end
+#
+#    def run(app)
+#      klass = app.class
+#      klass.instance_variable_set "@rack_builder", self
+#      def klass.rack_builder
+#        @rack_builder
+#      end
+#      @ins << app #lambda { |nothing| app }
+#    end
+#
+#    def leaf_app
+#      ins.last
+#    end
+#  end
+#end
 
 module Rack
   class Flash
